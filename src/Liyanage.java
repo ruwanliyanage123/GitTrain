c5b661a8322342c07fefe6d86b30c8d856d9f2fc
ruwanliyanagege123 <ruwanliyanagegalle1994@gmail.com>
2021-04-17 01:41:31 +0530

    init commit

---
+++ src/Liyanage.java
@@ -0,0 +1,8 @@
+public class Liyanage {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("nine commmmit");
+        System.out.println("nine commmmit");
+    }
+}
---
+++ src/Ruwan.java
@@ -0,0 +1,10 @@
+public class Ruwan {
+    public static void main(String[] args) {
+        System.out.println("ruwan");
+        System.out.println("ruwan");
+
+        for(int a=10; a<10; a++){
+            System.out.println("nine commmmit");
+        }
+    }
+}
